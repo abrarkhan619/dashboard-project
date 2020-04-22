f91bd8f757fa6896774c394f10a69b6c7dfd2b35
Abrar Khan <abrar.khan@hee.nhs.uk>
2020-04-22 15:58:20 +0100

    connected to MongoDB in connection.js file

---
+++ connection.js
@@ -0,0 +1,31 @@
+const {MongoClient} = require('mongodb');
+require('dotenv').config();
+
+async function main(){
+    // const uri = "mongodb+srv://abrar:pa55word@usersignup-ewwmc.mongodb.net/test?retryWrites=true&w=majority";
+    const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@usersignup-ewwmc.mongodb.net/test?retryWrites=true&w=majority`;
+    const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+ 
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+ 
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+ 
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+    // databasesList = await client.getDB("Dashoard-APIs");
+ 
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
---
+++ readme.md
@@ -0,0 +1,31 @@
+To run nodemon:
+1. Run Powershell as admin
+2. command: Get-ExecutionPolicy
+3. If restricted, command: Set-ExecutionPolicy remotesigned
+
+
+Services:
+Sonarcloud:
+- assessments
+- generic upload
+- tis profile
+- tis reference
+- sync
+- tcs
+- tis Admins UI
+
+Sentry
+- admins UI
+- Trainee UI
+- tis profile
+- tis trainee forms
+- tis trainee reference
+
+Jenkins
+- tcs
+- reference
+- admins UI
+- profile
+- assessments
+- user-management
+- ndw-etl
